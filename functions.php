8027d228b81d5da13260cba244e373d5d75d139c
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-18 14:32:52 +0800

    update styles

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * For additional information on potential customization options,
  * read the developers' documentation:
  *
- * https://developers.elementor.com/docs/tfb-elementor-theme/
+ * https://developers.elementor.com/docs/fin-elementor-theme/
  *
  * @package fin-theme
  */
@@ -24,8 +24,10 @@ define('FIN_THEME_VERSION', '1.1.1');
 
 function fin_theme_scripts_styles()
 {
-    wp_enqueue_style('tfb-theme-style', get_stylesheet_directory_uri() . '/style.css', [], FIN_THEME_VERSION);
-    wp_enqueue_style('tfb-theme-custom-style', get_stylesheet_directory_uri() . '/assets/css/tfb-theme.css', [], FIN_THEME_VERSION);
-    wp_enqueue_script('tfb-theme-custom-script', get_stylesheet_directory_uri() . '/assets/js/tfb-theme.js', [], FIN_THEME_VERSION, true);
+    wp_enqueue_style('fin-bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css', [], '5.3.0-alpha3');
+    wp_enqueue_style('fin-theme-style', get_stylesheet_directory_uri() . '/style.css', [], FIN_THEME_VERSION);
+    wp_enqueue_style('fin-theme-custom-style', get_stylesheet_directory_uri() . '/assets/css/finprtheme.css', [], FIN_THEME_VERSION);
+    wp_enqueue_script('fin-bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js', ['jquery'], '5.3.0-alpha3', true);
+    wp_enqueue_script('fin-theme-custom-script', get_stylesheet_directory_uri() . '/assets/js/finprtheme.js', [], FIN_THEME_VERSION, true);    
 }
 add_action('wp_enqueue_scripts', 'fin_theme_scripts_styles', 20);
